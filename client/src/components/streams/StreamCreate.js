9fa1b624857efd2eaa9fa622d844042368112142
Grisel Matos <griselmatosm@gmail.com>
2020-05-28 20:50:34 +0200

    #5 Display validation messages, show error messages on touch out of the field. Add styles highlighting errored fields

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label, meta }) {
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      )
+    }
+
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input type="text" {...input} /> {/*guarda el value y captura el evento */}
-        <div>{meta.error}</div>
+        <input type="text" {...input} autoComplete="off" /> {/*guarda el value y captura el evento */}
+        {this.renderError(meta)}
       </div>
     );
   }
@@ -19,7 +31,7 @@ class StreamCreate extends React.Component {
 
   render() {
     return (
-      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+      <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <Field name="title" component={this.renderInput} label="Enter title" />
         <Field name="description" component={this.renderInput} label="Enter description" />
         <button className="ui button primary">Create</button>
