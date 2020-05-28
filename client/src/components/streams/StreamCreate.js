b7154e5b3fa3b5fa38208b92729debbe1931b715
Grisel Matos <griselmatosm@gmail.com>
2020-05-28 19:25:50 +0200

    #5 Customize form fields

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-
-  renderInput({ input }) {
-    return <input type="text" {...input}/> //guarda el value y captura el evento
+  renderInput({ input, label }) {
+    return (
+      <div className="field"> 
+         <label>{label}</label> 
+         <input type="text" {...input} /> {/*guarda el value y captura el evento */}
+      </div>
+    );
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} /> 
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field name="description" component={this.renderInput} label="Enter description" />
       </form>
-    )
+    );
   }
 }
 
-export default reduxForm({ //reduxForm recibe un  solo objeto, con una clave form y valor un string que indique el propósito del formulario
-  form: 'streamCreate'
+export default reduxForm({
+  //reduxForm recibe un  solo objeto, con una clave form y valor un string que indique el propósito del formulario
+  form: 'streamCreate',
 })(StreamCreate);
