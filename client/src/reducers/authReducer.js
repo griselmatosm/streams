279f32959b01fff23ac89caee24e5ae30744014c
Grisel Matos <griselmatosm@gmail.com>
2020-05-28 16:20:26 +0200

    #4 Record the userId on the state of the app

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,6 +2,7 @@ import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
 const INITIAL_STATE = {
   isSignedIn: null,
+  userId: null
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -9,9 +10,9 @@ export default (state = INITIAL_STATE, action) => {
     
   switch (action.type) {
     case SIGN_IN:
-      return { ...state, isSignedIn: true };
+      return { ...state, isSignedIn: true, userId: action.payload };
     case SIGN_OUT:
-      return { ...state, isSignedIn: false };
+      return { ...state, isSignedIn: false, userId: null };
     default:
       return state;
   }
