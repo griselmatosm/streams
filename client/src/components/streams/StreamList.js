3061fd2940c5481cfe265a8405a2558ad7a67bb6
Grisel Matos <griselmatosm@gmail.com>
2020-06-01 11:30:52 +0200

    #8 Render the list of all streams in StreamList component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -6,9 +6,34 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderList = () => {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            <h5>{stream.title}</h5>
+            <div className="description">
+              <p>{ stream.description }</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>Stream List</div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{ this.renderList() }</div>
+      </div>
+    ) 
   }
 }
 
-export default connect(null, {fetchStreams})(StreamList);
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) }; //Object.values devuelve array de elementos corresponden a las propiedades enumerables de dicho objeto
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
