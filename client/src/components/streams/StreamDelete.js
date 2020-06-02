b46cc48bd8342cf4c783e3da363031f234ac97bb
Grisel Matos <griselmatosm@gmail.com>
2020-06-02 18:31:46 +0200

    #11 Change API records by deleting a stream

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -3,7 +3,7 @@ import Modal from '../Modal';
 import history from '../../history';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchStream } from '../../actions';
+import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
   componentDidMount() {
@@ -11,9 +11,14 @@ class StreamDelete extends React.Component {
   }
 
   renderActions() {
+    const { id } = this.props.match.params;
     return (
       <React.Fragment>
-        <button className="ui button negative">Delete</button>
+        <button 
+          className="ui button negative" 
+          onClick={() => this.props.deleteStream(id)}>
+          Delete
+        </button>
         <Link to="/" className="ui button">Cancel</Link>
       </React.Fragment>
     );
@@ -41,4 +46,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamDelete);
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
