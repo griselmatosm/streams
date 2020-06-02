505d798a447ad826f560fabd74563c8914572ca9
Grisel Matos <griselmatosm@gmail.com>
2020-06-02 18:21:59 +0200

    #11 Add Cancel functionality to cancel delete a stream

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,37 +2,43 @@ import React from 'react';
 import Modal from '../Modal';
 import history from '../../history';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStream } from '../../actions';
 
 class StreamDelete extends React.Component {
-
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id)    
+    this.props.fetchStream(this.props.match.params.id);
   }
-
 
   renderActions() {
     return (
       <React.Fragment>
         <button className="ui button negative">Delete</button>
-        <button className="ui button">Cancel</button>
+        <Link to="/" className="ui button">Cancel</Link>
       </React.Fragment>
     );
   }
 
+  renderContent() {
+    if (!this.props.stream) {
+      return 'Are you sure you want to delete this stream?';
+    }
+    return `Are you sure you want to delete this stream with title: ${this.props.stream.title}?`;
+  }
+
   render() {
     return (
-      <div>
-        Stream Delete
         <Modal 
           title="Delete Stream" 
-          description="Are you sure you want to delete this stream?" 
-          actions={this.renderActions()} 
-          onDismiss={() => history.push('/')} 
+          description={this.renderContent()} 
+          actions={this.renderActions()} onDismiss={() => history.push('/')} 
         />
-      </div>
     );
   }
 }
 
-export default connect(null, { fetchStream })(StreamDelete);
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamDelete);
